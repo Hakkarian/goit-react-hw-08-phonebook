21a3ddfd3f48200c3ae1148a41de29b12452f072
Hakkar <zugrutus@gmail.com>
2023-03-11 19:50:59 +0000

    Updates

---
+++ static/js/60.67ab4d2e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[60],{8060:function(n,e,t){t.r(e),t.d(e,{default:function(){return u}});var o,r=t(4169),c=t(168),f=t(6088).default.h1(o||(o=(0,c.Z)(["\n    color: #fff;\n    font-size: 48px;\n    font-weight: 500;\n"]))),h=t(184),u=function(){return(0,h.jsx)(r.Yx,{children:(0,h.jsx)(f,{children:"Welcome, stranger!"})})}}}]);
+//# sourceMappingURL=60.67ab4d2e.chunk.js.map
